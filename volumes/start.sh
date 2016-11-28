e6f15641066fbb07e8bd9af3312ffe0944b81e81
mureevms <m.mureev@anchorfree.com>
2016-11-28 23:24:18 +0700

    edit config

--- volumes/start.sh
+++ volumes/start.sh
@@ -5,9 +5,9 @@ if [ ! -f /etc/openvpn/keys/ta.key ]; then
     openvpn --genkey --secret ta.key
 fi
 iptables -A INPUT -p UDP --dport 1194 -j ACCEPT
+iptables -A INPUT -s 10.15.0.0/24 -j ACCEPT
 iptables -A FORWARD -s 10.15.0.0/24 -j ACCEPT
 iptables -A FORWARD -d 10.15.0.0/24 -j ACCEPT
-# If used as a default gateway
-#iptables -t nat -A POSTROUTING -s 10.15.0.0/24 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 10.15.0.0/24 -j MASQUERADE
 
 exec openvpn --config /etc/openvpn/server.conf
